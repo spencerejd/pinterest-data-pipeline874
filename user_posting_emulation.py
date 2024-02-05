88df1b386fc3d9a73476a82756da87d8e2b0dbe9
Spencer Duvwiama <ejduv@yahoo.co.uk>
2024-02-05 14:12:36 +0000

    Updated methods

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -11,14 +11,14 @@ from datetime import datetime
 
 random.seed(100)
 
-# class DateTimeEncoder(json.JSONEncoder):
-#     """ Custom encoder for datetime objects """
-#     def default(self, obj):
-#         if isinstance(obj, datetime):
-#             # Format datetime objects as strings
-#             return obj.isoformat()
-#         # Let the base class default method raise the TypeError
-#         return json.JSONEncoder.default(self, obj)
+class DateTimeEncoder(json.JSONEncoder):
+    """ Custom encoder for datetime objects """
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            # Format datetime objects as strings
+            return obj.isoformat()
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
 
 
 class AWSDBConnector:
@@ -39,10 +39,10 @@ class AWSDBConnector:
 new_connector = AWSDBConnector()
 
 def send_to_api_gateway(data, topic_name):
-    invoke_url = f"https://rfrcndbjtb.execute-api.us-east-1.amazonaws.com/prod/{topic_name}"
+    invoke_url = f"https://rfrcndbjtb.execute-api.us-east-1.amazonaws.com/prod/topics/{topic_name}"
     headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
     # Prepare payload
-    payload = json.dumps({"records": [{"value": data}]})
+    payload = json.dumps({"records": [{"value": data}]}, cls=DateTimeEncoder)
     # Send POST request
     response = requests.post(invoke_url, headers=headers, data=payload)
     return response
@@ -65,25 +65,25 @@ def run_infinite_post_data_loop():
             response = send_to_api_gateway(pin_result, "0a1667ad2f7f.pin")
             print(f"Pin Response: {response.status_code}, {response.text}")
 
-            # # Fetch and send data from geolocation_data table
-            # geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
-            # geo_selected_row = connection.execute(geo_string)
+            # Fetch and send data from geolocation_data table
+            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+            geo_selected_row = connection.execute(geo_string)
             
-            # for row in geo_selected_row:
-            #     geo_result = dict(row._mapping)
+            for row in geo_selected_row:
+                geo_result = dict(row._mapping)
 
-            # response = send_to_api_gateway(geo_result, "0a1667ad2f7f.geo")
-            # print(f"Geo Response: {response.status_code}, {response.text}")
+            response = send_to_api_gateway(geo_result, "0a1667ad2f7f.geo")
+            print(f"Geo Response: {response.status_code}, {response.text}")
 
-            # # Fetch and send data from user_data table
-            # user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
-            # user_selected_row = connection.execute(user_string)
+            # Fetch and send data from user_data table
+            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+            user_selected_row = connection.execute(user_string)
             
-            # for row in user_selected_row:
-            #     user_result = dict(row._mapping)
+            for row in user_selected_row:
+                user_result = dict(row._mapping)
             
-            # response = send_to_api_gateway(user_result, "0a1667ad2f7f.user")
-            # print(f"User Response: {response.status_code}, {response.text}")
+            response = send_to_api_gateway(user_result, "0a1667ad2f7f.user")
+            print(f"User Response: {response.status_code}, {response.text}")
 
 
 
